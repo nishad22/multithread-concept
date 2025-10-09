c9e3d20c5f75c7142e037e9766a278de7ae22688
nishad <nishad9689@gmail.com>
2025-10-09 08:56:15 +0530

    fork-join example

---
+++ src/concurrent_collection/ArrayBlockingQueueExample.java
@@ -0,0 +1,12 @@
+package concurrent_collection;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class ArrayBlockingQueueExample {
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/method_of_thread/App.java
+++ src/method_of_thread/App.java
@@ -4,8 +4,8 @@ public class App{
     public static void main(String[] args) {
         var producerConsumer = new ProducerConsumerPattern();
 
-        var v1 = new Thread(new ProducerConsumerPattern.Producer(producerConsumer));
-        var v2 =  new Thread(new ProducerConsumerPattern.Consumer(producerConsumer));
+        var v1 = new Thread(new Producer(producerConsumer));
+        var v2 =  new Thread(new Consumer(producerConsumer));
 
         v1.start();
         v2.start();
